ff760ad0f367390500c85dd21e92e09c65e5157d
Woximus <wiktor.wlodarczyk006@gmail.com>
2026-03-11 22:07:32 +0100

    Add files via upload

---
+++ Widok-Kontroler/calc_view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Kalkulator Kredytowy</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="card">
+    <h2>Kalkulator</h2>
+    
+    <form action="calc.php" method="GET">
+        <label>Kwota kredytu:</label>
+        <input type="text" name="kwota" value="<?php echo $kwota; ?>"><br>
+
+        <label>Okres (lata):</label>
+        <input type="text" name="lata" value="<?php echo $lata; ?>"> <br>
+
+        <label>Oprocentowanie (%):</label>
+        <input type="text" name="oprocentowanie" value="<?php echo $procent; ?>"> <br>
+
+        <button type="submit">Oblicz ratę</button>
+    </form>
+
+    <div class="error">
+        <?php
+            if (isset($error) && $error != "") {
+                echo $error;
+            }
+        ?>
+    </div>
+
+    <div class="result">
+        <?php
+            if (isset($wynik) && $error == "") {
+                echo "Miesięczna rata: " . number_format($wynik, 2, ',', ' ') . " PLN<br>";
+                echo "Suma do spłaty: " . number_format($calkowita_kwota, 2, ',', ' ') . " PLN";
+            }
+        ?>
+    </div>
+</div>
+
+</body>
+</html>
