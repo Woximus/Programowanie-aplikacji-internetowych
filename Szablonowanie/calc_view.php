c29ad91677397b31a7859afa61417b2224cd6886
Woximus <wiktor.wlodarczyk006@gmail.com>
2026-03-16 20:49:33 +0100

    Add files via upload

---
+++ Szablonowanie/calc_view.php
@@ -0,0 +1,38 @@
+
+<div class="card">
+    <h2>Kalkulator</h2>
+    
+    <form action="calc.php" method="GET">
+        <label>Kwota kredytu:</label>
+        <input type="text" name="kwota" value="<?php echo $kwota; ?>"><br>
+
+        <label>Okres (lata):</label>
+        <input type="text" name="lata" value="<?php echo $lata; ?>"> <br>
+
+        <label>Oprocentowanie (%):</label>
+        <input type="text" name="oprocentowanie" value="<?php echo $procent; ?>"> <br>
+
+        <button type="submit" class="btn-calc">Oblicz ratę</button>
+    </form>
+
+    <div class="error">
+        <?php
+			if (isset($errors) && count($errors) > 0) {
+                echo "<ul>";
+                foreach ($errors as $err) {
+                    echo "<li>$err</li>";
+                }
+                echo "</ul>";
+            }
+        ?>
+    </div>
+
+    <div class="result">
+        <?php
+            if (isset($wynik) && empty($errors))	 {
+                echo "Miesięczna rata: " . number_format($wynik, 2, ',', ' ') . " PLN<br>";
+                echo "Suma do spłaty: " . number_format($calkowita_kwota, 2, ',', ' ') . " PLN";
+            }
+        ?>
+    </div>
+</div>
